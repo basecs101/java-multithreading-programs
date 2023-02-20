bbee0e0397aa83459a622b29c8ca57f4bfb60129
Vikram Gupta <basecs101@gmail.com>
2023-02-20 09:33:58 +0530

    1. Update ArrayBlockingQueue - Fix put and take for blocking queue. 2. Executor Service.

--- producer-consumer-using-blocking-queue/src/Main.java
+++ producer-consumer-using-blocking-queue/src/Main.java
@@ -2,27 +2,51 @@ import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
     public static void main(String[] args) {
 
-        BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(100,true);
+        BlockingQueue<Integer> blockingQueue = new ArrayBlockingQueue<>(1);
 
         Random random = new Random();
         Runnable producerRunnable = new Runnable() {
             @Override
             public void run() {
                 while (true){
-                    int i = random.nextInt();
-                    blockingQueue.add(i);
-                    System.out.println("Producer Thread produced : "+i);
+                    int i = random.nextInt(100);
+                    try {
+                        blockingQueue.put(i);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(Thread.currentThread().getName()+" produced : "+i);
+                    try {
+                        Thread.sleep(random.nextInt(1000));
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        };
+
+        Runnable consumerRunnable = new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        Integer i = blockingQueue.take();
+                        System.out.println(Thread.currentThread().getName()+" consumed : "+i);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                     try {
                         Thread.sleep(random.nextInt(2000));
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
                 }
-
             }
         };
 
@@ -31,27 +55,9 @@ public class Main {
         producer.setName("ProducerThread");
         producer.start();
 
-        Runnable consumerRunnable = new Runnable() {
-            @Override
-            public void run() {
-                while (true){
-                    try {
-                        Integer i = blockingQueue.take();
-                        System.out.println("Consumer Thread consumed : "+i);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                    try {
-                        Thread.sleep(random.nextInt(2000));
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        };
-
         Thread consumer = new Thread(consumerRunnable);
         consumer.setName("ConsumerThread");
         consumer.start();
+
     }
 }
